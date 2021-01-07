ce21107f7c154e8254552c98535c91869c98945e
SyedAnasAlam <saa0380@gmail.com>
2021-01-07 23:04:32 +0100

    PWM controlling servo + fan

--- include/HAL.h
+++ include/HAL.h
@@ -4,4 +4,8 @@
 #define POTENTIOMETER_PIN 4
 #define ADC_PRESCALER 0x07  // ADC cloc prescaler = 128 -> 125kHz
 
+#define SERVO_PIN 3
+#define FAN_PIN 6
+
 void InitADC();
+void InitPWM();
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,34 @@
+//TODO different map function (possibly without using FPU)
+//TODO cleanup
+
 #include <Arduino.h>
 #include "LCD.h"
 #include "HAL.h"
 
 volatile int temp = 0;
-volatile int pot = 0;
-char x[4];
+volatile float pot = 0;
+
+float map(float value, float start1, float stop1, float start2, float stop2)
+{
+    return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1)); 
+}
 
 void setup() 
 {
     LCDSetup();
     InitADC();
+    InitPWM();
    
     Serial.begin(9600);
 }
 
 void loop() 
 {  
-    LCDPrintMenu(temp, pot);
-    delay(100);   
+    //OCR0A = map(pot, 0, 1023, 0, 255);
+    OCR2B = 4;
+    delay(2000);
+    OCR2B = 19;
+    delay(2000);
 }
 
 ISR(ADC_vect)
